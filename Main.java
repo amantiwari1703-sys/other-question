1fa74038dbba8e2b113ca3ecbbba766fb923ac9d
Aman Tiwari <amantiwari@gmail.com>
2026-04-21 22:08:24 +0530

    input

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Demo d=new Demo();
-        d.msg();
-
+        System.out.println("Enter the number");
+        int num=d.getNum();
+        System.out.println(num);
     }
 }
-interface message{
-    void msg();
-
+interface input{
+    int getNum();
 }
-class Demo implements message{
-    public void msg(){
-        System.out.println("welcome to java ");
+class Demo implements input{
+    public int getNum(){
+        Scanner sc=new Scanner(System.in);
+        return sc.nextInt();
     }
 }
