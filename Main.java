31aa71908a26956eeb5fcb025ebb8a90fc57fb73
Aman Tiwari <amantiwari@gmail.com>
2026-04-21 22:51:40 +0530

    cube

--- Main.java
+++ Main.java
@@ -1,19 +1,15 @@
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Demo d=new Demo();
-        System.out.println(d.area(5,3));
-    }
+        System.out.println(d.getCube(3));
 
-
-}
-interface Rectangle{
-    int area(int l,int b);
-
-}
-class Demo implements Rectangle{
-    public int area (int l,int b){
-        return l*b;
     }
 }
-
-
+interface Cube{
+    int getCube(int n);
+}
+class Demo implements Cube{
+    public int getCube(int n){
+        return n*n*n;
+    }
+}
