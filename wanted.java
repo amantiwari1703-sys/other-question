8f204c1ada1602ab5bb03c5d2b4cd0fe2590c1a0
Aman Tiwari <amantiwari@gmail.com>
2026-04-23 08:53:48 +0530

    enum

---
+++ wanted.java
@@ -0,0 +1,19 @@
+public class wanted {
+    public static void main(String[] args){
+        Level myVar=  Level.MEDIUM;
+        System.out.println(myVar);
+
+    }
+
+}
+enum Level{
+    LOW,
+    MEDIUM,
+    HIGH,
+
+}
+
+
+
+
+
---
+++ writer.java
@@ -0,0 +1,15 @@
+public class writer {
+    public static void main(String[] args){
+        Book b=Book.high;
+        System.out.println(b);
+
+
+    }
+
+
+enum Book{
+    high,
+    MEDIUM,
+    LOWER,
+    }
+}
