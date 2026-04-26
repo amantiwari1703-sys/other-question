ca21eef6b4769c6aef3ca2602ec8a7ab8e6bb9fe
Aman Tiwari <amantiwari@gmail.com>
2026-04-26 23:29:26 +0530

    file

--- writer.java
+++ writer.java
@@ -1,15 +1,12 @@
+import java.io.FileWriter;
+import java.io.File;
 public class writer {
-    public static void main(String[] args){
-        Book b=Book.high;
-        System.out.println(b);
-
-
-    }
-
-
-enum Book{
-    high,
-    MEDIUM,
-    LOWER,
+    public static void main(String[] args) {
+        File myObj = new File("index.html");
+        if (myObj.delete()) {
+            System.out.println("Deleted the file: " + myObj.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
+        }
     }
 }
